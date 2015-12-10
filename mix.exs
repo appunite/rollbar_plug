c872b2c1a372ef0d285053286a5768cfde5de682
Rafal Radziszewski <rafal.radziszewski@appunite.com>
2015-12-10 18:15:22 +0100

    Bump, works as expected

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RollbarPlug.Mixfile do
 
   def project do
     [app: :rollbar_plug,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
